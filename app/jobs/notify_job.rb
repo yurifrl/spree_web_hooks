b7bfff591583df2403c96aadaf56d49b41ccee70
yurifrl <yurifrl@hotmail.com>
2015-01-29 14:26:55 -0200

    let the gem handle it

--- app/jobs/notify_job.rb
+++ app/jobs/notify_job.rb
@@ -1,7 +1,7 @@
 class NotifyJob < ActiveJob::Base
   queue_as :default
 
-  def perform(action, model, database)
+  def perform(action, model)
     name = model.class.name.split('::').last.underscore
     Spree::WebHooks::Hook.joins(:event).where(spree_web_hooks_events: { name: "#{name}_#{action}" }).each do |e|
       e.notify(model)
